78f6b80293c239adba9a0af06562b19accd951bb
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-02 21:12:00 -0200

    remove items class

--- src/domain/services/SystemService.js
+++ src/domain/services/SystemService.js
@@ -1,32 +1,14 @@
 import os from 'os'
 
-const __classes = {
-    // User 1's classes.
-    1: [{
-      id: 1,
-      name: 'ES7 for Dummies'
-    }, {
-      id: 2,
-      name: 'Koa 101'
-    }],
-  
-    // User 2's classes
-    2: [{
-      id: 3,
-      name: 'Dependency Injection with Awilix'
-    }]
-  }
-  
-  export default class ClassService {
-    
-    async description(){
-      let jsonInf = {
-        "cpu" : os.cpus(),
-        "hostname": os.hostname(),
-        "homedir": os.homedir()
-      }
-      return jsonInf
-    }
+export default class ClassService {
 
+  async description() {
+    let jsonInf = {
+      "cpu": os.cpus(),
+      "hostname": os.hostname(),
+      "homedir": os.homedir()
+    }
+    return jsonInf
   }
-  
+
+}
