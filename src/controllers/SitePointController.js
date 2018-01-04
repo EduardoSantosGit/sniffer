80617c46ca9cb97717c13dfa812cc316a86e45fc
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-03 22:52:40 -0200

    add class service and controller site

---
+++ src/controllers/SitePointController.js
@@ -0,0 +1,27 @@
+import {
+    makeClassInvoker
+  } from 'awilix-koa'
+
+import  dns from 'dns'
+
+  class SitePointController {
+    constructor ({
+        sitesService
+    }) {
+      this.sitesService = sitesService 
+    }
+  
+    async getInfoSite (ctx) {   
+        const site = ctx.params.site
+        ctx.body = {
+            "inf" : await this.sitesService.getInfoSite(site)
+        } 
+    }    
+  }
+  
+  export default function (router) {
+    // Same trick as the functional API, but using `makeClassInvoker`.
+    const api = makeClassInvoker(SitePointController)
+  
+    router.get('/sites/info/:site', api('getInfoSite'))
+  }
---
+++ src/domain/services/SitesService.js
@@ -0,0 +1,21 @@
+import dns from 'dns'
+
+export default class SitesService {
+
+    async getServers(site) {
+
+        return dns.resolve4('site', (err, addresses) => {
+            if (err) throw err;
+            
+            addresses.forEach((a) => {
+                dns.reverse(a, (err, hostnames) => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+            });
+        });
+
+    }
+
+}
