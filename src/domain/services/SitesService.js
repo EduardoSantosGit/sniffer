eeb1f60347a5375633dbbd002bf56976af29ecd5
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-04 22:42:14 -0200

    implement ip server site

--- src/domain/services/SitesService.js
+++ src/domain/services/SitesService.js
@@ -3,19 +3,15 @@ import dns from 'dns'
 export default class SitesService {
 
     async getServers(site) {
-
-        return dns.resolve4(site, (err, addresses) => {
-            if (err) throw err;
-
-            addresses.forEach((a) => {
-                dns.reverse(a, (err, hostnames) => {
-                    if (err) {
-                        throw err;
-                    }
-                });
-            });
-        });
-
+        
+        return new Promise(function (resolve, reject) {
+            dns.lookup(site, (err, address, family) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(address)
+            })
+        })
     }
 
 }
