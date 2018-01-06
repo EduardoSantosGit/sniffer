200c8fb57c010e1c1cca76236075d3ff9cab12bf
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-05 23:01:27 -0200

    add method reverse ip

--- src/controllers/SitePointController.js
+++ src/controllers/SitePointController.js
@@ -1,27 +1,26 @@
 import {
-    makeClassInvoker
-  } from 'awilix-koa'
+  makeClassInvoker
+} from 'awilix-koa'
 
-import  dns from 'dns'
 
-  class SitePointController {
-    constructor ({
+class SitePointController {
+  constructor({
         sitesService
     }) {
-      this.sitesService = sitesService 
-    }
-  
-    async getInfoSite (ctx) {   
-        const site = ctx.params.site
-        ctx.body = {
-            "inf" : await this.sitesService.getServers(site)
-        } 
-    }    
+    this.sitesService = sitesService
   }
-  
-  export default function (router) {
-    // Same trick as the functional API, but using `makeClassInvoker`.
-    const api = makeClassInvoker(SitePointController)
-  
-    router.get('/sites/info/:site', api('getInfoSite'))
+
+  async getInfoSite(ctx) {
+    const site = ctx.params.site
+    ctx.body = {
+      "inf": await this.sitesService.getServers(site)
+    };
   }
+}
+
+export default function (router) {
+  // Same trick as the functional API, but using `makeClassInvoker`.
+  const api = makeClassInvoker(SitePointController)
+
+  router.get('/sites/info/:site', api('getInfoSite'))
+}
--- src/domain/services/SitesService.js
+++ src/domain/services/SitesService.js
@@ -3,13 +3,23 @@ import dns from 'dns'
 export default class SitesService {
 
     async getServers(site) {
-        
         return new Promise(function (resolve, reject) {
             dns.lookup(site, (err, address, family) => {
                 if (err) {
                     reject(err)
                 }
                 resolve(address)
+            })
+        })
+    }
+
+    async reverse(ip){
+        return new Promise(function (resolve, reject) {
+            dns.reverse(ip, (err, hostnames) => {
+                if (err) {
+                    reject(err)
+                }
+                resolve(hostnames)
             })
         })
     }
