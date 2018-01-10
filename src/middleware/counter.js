d83f125daf298d9b765b4667ee88f47638c32d82
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-09 22:26:06 -0200

    implement set ctx count middleware

--- src/middleware/counter.js
+++ src/middleware/counter.js
@@ -3,7 +3,8 @@ export default function (opts) {
     let count = 0;
 
     return async function ipInfo(ctx, next) { 
-        console.log(count++)
+        console.log("request " + count++)
+        ctx.set("count", count)
         next()
     }
 }
