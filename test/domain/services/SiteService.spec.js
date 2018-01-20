568b4a9ec3dc5faf02232faafa794f5dcf322b0a
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-19 23:39:56 -0200

    add method test getbasicsite

--- test/domain/services/SiteService.spec.js
+++ test/domain/services/SiteService.spec.js
@@ -1,6 +1,16 @@
-import DataSiteService from '../../../src/domain/services/SiteService'
+import SiteService from '../../../src/domain/services/SiteService'
 import { expect } from 'chai'
 
 describe('SiteService service', function () {
+
+    it('test method getDataBasicSite when site returns OK and family ip', async function () {
+            
+        let siteService = new SiteService();
+        let result = await siteService.getDataBasicSite("www.google.com");
+        
+        expect(result.status).to.equal("OK");
+        expect(result.value.name).to.equal("www.google.com");
+        expect(result.value.family).to.equal(4);
+    })
 
 });
