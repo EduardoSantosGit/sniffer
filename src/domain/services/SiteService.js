58cf438fc1a5d7f28fc0d1be78fb8bfa211dfd96
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-19 23:24:29 -0200

    add method and instance service

--- src/domain/services/SiteService.js
+++ src/domain/services/SiteService.js
@@ -1,7 +1,13 @@
+import DataSiteService from '../../infrastructure/services/DataSiteService'
+
 export default class SiteService {
 
     constructor(){
-        
+        this.dataSiteService = new DataSiteService()
+    }
+
+    async getDataBasicSite(site){
+           
     }
 
 }
