19140483d6eb21562903bf6f2fddc5c9aed3269a
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-22 13:07:41 -0200

    add extends class add atributte hostname

--- src/domain/models/SiteHost.js
+++ src/domain/models/SiteHost.js
@@ -2,9 +2,22 @@ export default class SiteHost extends Site{
 
     constructor()
     {
+        if (json == null || json == undefined)
+            return;
 
+        this.name = json.name
+        this.family = json.family
+        this.ip = json.ip
+
+        super(this.name, this.family, this.ip)
     }
 
-    
+    get hostName(){
+        return this._hostName
+    }
+
+    set hostName(value){
+        this._hostName = value
+    }
 
 }
