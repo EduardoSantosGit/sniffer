a0f84d559edd2e5e6b92c34ee67cc72c44b00dfc
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-22 22:51:20 -0200

    add verify success status controller

--- src/domain/common/BaseController.js
+++ src/domain/common/BaseController.js
@@ -1,7 +1,20 @@
 export default class BaseController {
 
     static createResponseFromResult(ctx, result, resourceName = null) {
-        
+
+        if (result.status === "OK") {
+            ctx.status = 200;
+            if (Array.isArray(result.value)) {
+              ctx.body = {
+                "totalCount": (result.value || []).length,
+              };
+              
+              if (!resourceName)
+                resourceName = "items";
+      
+              ctx.body[resourceName] = result.value;
+            }
+        }    
     }
 
 }
