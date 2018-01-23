f62b7186cb6e31e92d4820d444cff1b1f759629a
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-22 23:12:34 -0200

    add logger domain services

--- src/domain/services/SiteService.js
+++ src/domain/services/SiteService.js
@@ -3,6 +3,7 @@ import Site from '../models/Site'
 import SiteHost from '../models/SiteHost'
 import Result from '../common/Result'
 import Cache from './CacheService'
+import logger from '../../lib/logger'
 
 export default class SiteService {
 
@@ -11,11 +12,13 @@ export default class SiteService {
     }
 
     async getDataBasicSite(site){
+        outLogger("getDataBasicSite", site)
         let result = await this.dataSiteService.getLookup(site);      
         return new Result("OK", new Site({ name : site, family: result[1], ip : result[0] }));
     }
 
     async getDataCompleteSite(site){
+        outLogger("getDataCompleteSite", site)
         let resultIp = await this.dataSiteService.getResolve4(site);
 
         let ret = []
@@ -28,6 +31,16 @@ export default class SiteService {
         }));
 
         return new Result("OK", new SiteHost({ name : site, family: 4, ip: resultIp, hosts: ret}));
+    }
+
+    outLogger(method, param){
+        logger.debug({
+            class: 'SiteService',
+            method: method,
+            params: {
+                param
+            }
+        })
     }
 
 }
