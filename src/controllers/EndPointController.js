52e1f0e286daec31aa719bf2068004891f53888b
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-22 23:16:55 -0200

    add base controller info

--- src/controllers/EndPointController.js
+++ src/controllers/EndPointController.js
@@ -1,4 +1,5 @@
 import { makeClassInvoker } from 'awilix-koa'
+import Base from '../domain/common/BaseController'
 
 class EndpointController {
   
@@ -7,10 +8,15 @@ class EndpointController {
   }
 
   async getInfo (ctx) {
-    ctx.ok({
+    /*ctx.ok({
       timestamp: new Date(),
       info: await this.systemService.getInfoSystem()
-    })
+    })*/
+    Base.createResponseFromResult(ctx,
+      {
+      timestamp: new Date(),
+      info: await this.systemService.getInfoSystem()
+    }, "info")
   }
 
 }
