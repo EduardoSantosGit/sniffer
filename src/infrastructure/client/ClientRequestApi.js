04c7ea35dfed15eae27b9f4d9240db296a44209e
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-23 13:23:52 -0200

    add method get request

--- src/infrastructure/client/ClientRequestApi.js
+++ src/infrastructure/client/ClientRequestApi.js
@@ -2,4 +2,15 @@ import request from 'request'
 
 export default class ClientRequestApi {
 
+    _RequestMethodGet(url){
+        return new Promise(function(resolve, reject){
+            request.get(url, function (error, response, body) {
+                if(error)
+                    reject(error)
+                
+                resolve([response, body])    
+            });
+        })   
+    }
+
 }
