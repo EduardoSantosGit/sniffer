31081aee394530d8d00c50162e93f5223aefe9d8
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-26 13:23:57 -0200

    add method test resolvehttp

--- test/infrastructure/services/DataSiteRequestTest.spec.js
+++ test/infrastructure/services/DataSiteRequestTest.spec.js
@@ -16,11 +16,29 @@ describe('DataSiteRequestService service', function () {
     it('test method getResponseHeaderSite not content protocol returns response and body', async function () {
     
         let dataSiteRequestService = new DataSiteRequestService();
-        let result = await dataSiteRequestService.getResponseHeaderSite("www.npmjs.com");
+        let result = await dataSiteRequestService.getResponseHeaderSite("www.npmjs.com", "https");
         
         expect(result).to.not.null
         expect(result.status).to.equal("OK")
         expect(result.value).to.not.null
     })
- 
+    
+    it('test method resolveUrlRequest protocol http returns url complete http', function () {
+    
+        let dataSiteRequestService = new DataSiteRequestService();
+        let result = dataSiteRequestService.resolveUrlRequest("www.npmjs.com", "http");
+        
+        expect(result).to.not.null
+        expect(result).to.equal("http://www.npmjs.com")
+    })
+
+    it('test method resolveUrlRequest protocol https returns url complete https', function () {
+    
+        let dataSiteRequestService = new DataSiteRequestService();
+        let result = dataSiteRequestService.resolveUrlRequest("www.npmjs.com", "https");
+        
+        expect(result).to.not.null
+        expect(result).to.equal("https://www.npmjs.com")
+    })
+
 });
