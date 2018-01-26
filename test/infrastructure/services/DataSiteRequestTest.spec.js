df65b457db16c33ffc065e630f96a68d237f1e81
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-26 13:17:40 -0200

    adjust test service request and add test protocol

--- test/infrastructure/services/DataSiteRequestTest.spec.js
+++ test/infrastructure/services/DataSiteRequestTest.spec.js
@@ -3,12 +3,24 @@ import { expect } from 'chai'
 
 describe('DataSiteRequestService service', function () {
 
-    it('test method getResponseHeaderSite returns response and body', async function () {
+    it('test method getResponseHeaderSite content protocol returns response and body', async function () {
     
         let dataSiteRequestService = new DataSiteRequestService();
         let result = await dataSiteRequestService.getResponseHeaderSite("https://www.npmjs.com");
         
         expect(result).to.not.null
+        expect(result.status).to.equal("OK")
+        expect(result.value).to.not.null
+    })
+
+    it('test method getResponseHeaderSite not content protocol returns response and body', async function () {
+    
+        let dataSiteRequestService = new DataSiteRequestService();
+        let result = await dataSiteRequestService.getResponseHeaderSite("www.npmjs.com");
+        
+        expect(result).to.not.null
+        expect(result.status).to.equal("OK")
+        expect(result.value).to.not.null
     })
  
 });
