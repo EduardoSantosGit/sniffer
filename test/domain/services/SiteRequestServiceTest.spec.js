b50656751590acda7c8d7c601605deef6adf6654
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-26 13:30:05 -0200

    add method test service domain return model

--- test/domain/services/SiteRequestServiceTest.spec.js
+++ test/domain/services/SiteRequestServiceTest.spec.js
@@ -3,12 +3,24 @@ import { expect } from 'chai'
 
 describe('SiteRequestService service', function () {
 
-    it('test method getDataSiteHeader return ok and header', async function () {
+    it('test method getDataSiteHeader content protocol return ok and header', async function () {
             
         let siteRequestService = new SiteRequestService();
         let result = await siteRequestService.getDataSiteHeader("https://www.npmjs.com")
         
+        expect(result).to.not.null
         expect(result.status).to.equal("OK");
+        expect(result.value.date).to.not.null
+    })
+
+    it('test method getDataSiteHeader not content protocol return ok and header', async function () {
+            
+        let siteRequestService = new SiteRequestService();
+        let result = await siteRequestService.getDataSiteHeader("www.npmjs.com", "https")
+        
+        expect(result).to.not.null
+        expect(result.status).to.equal("OK");
+        expect(result.value.date).to.not.null
     })
 
 });
