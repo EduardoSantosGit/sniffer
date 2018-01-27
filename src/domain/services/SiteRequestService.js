5c1b5e44cc6c3425912322474d1b1bb4b5ae9ae4
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-27 19:50:28 -0200

    alter indent tests and add .editorconfig

--- src/domain/services/SiteRequestService.js
+++ src/domain/services/SiteRequestService.js
@@ -4,24 +4,23 @@ import Header from '../models/Header'
 
 export default class SiteRequestService {
 
-    constructor(){
+    constructor() {
         this.dataSiteRequestService = new DataSiteRequestService()
     }
 
-    async getDataSiteHeader(site, protocol){
+    async getDataSiteHeader(site, protocol) {
         let header = await this.dataSiteRequestService.getResponseHeaderSite(site, protocol)
-        
-        if(header.status == "OK")
-        {
+
+        if (header.status == "OK") {
             let jsonHeader = {
-                date : header.value['date'],
-                connection : header.value['connection'],
-                contentEncoding : header.value['content-encoding'],
-                contentType : header.value['content-type'],
-                transferEncoding : header.value['Transfer-Encoding'],
-                acceptRanges : header.value['Accept-Ranges']
+                date: header.value['date'],
+                connection: header.value['connection'],
+                contentEncoding: header.value['content-encoding'],
+                contentType: header.value['content-type'],
+                transferEncoding: header.value['Transfer-Encoding'],
+                acceptRanges: header.value['Accept-Ranges']
             }
-            
+
             return new Result("OK", new Header(jsonHeader))
         }
 
--- src/infrastructure/services/DataSiteHostService.js
+++ src/infrastructure/services/DataSiteHostService.js
@@ -2,56 +2,56 @@ import dns from 'dns'
 import logger from '../../lib/logger'
 
 export default class DataSiteHostService {
-    
-    async getLookup(site){
+
+    async getLookup(site) {
         this.outLogger("getLookup", site)
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             dns.lookup(site, (err, address, family) => {
-                if(err)
+                if (err)
                     reject(err)
-                
+
                 resolve([address, family])
             });
         });
     }
-    
-    async getResolve4(site){
+
+    async getResolve4(site) {
         this.outLogger("getResolve4", site)
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             dns.resolve4(site, (err, addresses) => {
-                if (err) 
+                if (err)
                     reject(err);
 
-                resolve(addresses)                          
-              });
+                resolve(addresses)
+            });
         });
     }
 
-    async getResolve6(site){
+    async getResolve6(site) {
         this.outLogger("getResolve6", site)
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             dns.resolve6(site, (err, addresses) => {
-                if (err) 
+                if (err)
                     reject(err);
 
-                resolve(addresses)                          
-              });
+                resolve(addresses)
+            });
         });
     }
-    
-    async getReverse(addresses){
+
+    async getReverse(addresses) {
         this.outLogger("getReverse", addresses)
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             dns.reverse(addresses, (err, hostnames) => {
                 if (err) {
-                   reject(err);
+                    reject(err);
                 }
-                resolve(hostnames);               
+                resolve(hostnames);
             });
         })
     }
 
-    outLogger(method, param){
+    outLogger(method, param) {
         logger.debug({
             class: 'DataSiteService',
             method: method,
