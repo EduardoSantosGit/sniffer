f77c94eb95ff5aab7e640dba7ce4beb8b294432b
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-28 11:31:21 -0200

    bug trim variable node_env adjust

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -3,7 +3,7 @@ import env from '../lib/env'
 
 let logger = new winston.Logger();
 
-if(env.NODE_ENV !== 'test')
+if(env.NODE_ENV.trim() !== 'test')
   logger.add(winston.transports.Console, {
       name: 'console.info',
       colorize: true,
