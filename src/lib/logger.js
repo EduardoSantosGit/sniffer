051c1665690a70e995bd0b058236938b3bffd4fa
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-27 22:12:44 -0200

    alter order if logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -3,13 +3,11 @@ import env from '../lib/env'
 
 let logger = new winston.Logger();
 
-logger.add(winston.transports.Console, {
-    name: 'console.info',
-    colorize: true,
-    showLevel: true
-})
-
-if(env.NODE_ENV === 'test')
-  logger.transports['console.info'].silent = true 
+if(env.NODE_ENV !== 'test')
+  logger.add(winston.transports.Console, {
+      name: 'console.info',
+      colorize: true,
+      showLevel: true
+  })
 
 export default logger
