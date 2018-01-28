77618f5f4669dc9ed615c0cc6147e5663af4e642
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-27 22:11:34 -0200

    add logger config test

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,10 +1,15 @@
 import winston from 'winston' 
+import env from '../lib/env'
 
-let logger = new winston.Logger({
-  level: 'info',
-  transports: [
-    new (winston.transports.Console)(),
-  ]
-});
+let logger = new winston.Logger();
+
+logger.add(winston.transports.Console, {
+    name: 'console.info',
+    colorize: true,
+    showLevel: true
+})
+
+if(env.NODE_ENV === 'test')
+  logger.transports['console.info'].silent = true 
 
 export default logger
