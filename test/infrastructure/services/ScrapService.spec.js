7a0f836eef8d2e2d3c5d9ee7be04d9a083f218b7
EduardoSantosGit <eduardosantos058@gmail.com>
2018-02-08 13:16:22 -0200

    add test method parse css with site not .css

--- test/infrastructure/services/ScrapService.spec.js
+++ test/infrastructure/services/ScrapService.spec.js
@@ -12,5 +12,17 @@ describe('Scrap service', function () {
 
         let links = await scrapService.parseBlockCss(body);
         expect(links).to.not.null
+        expect(links[0].includes("css")).to.true;
+    })
+
+    it('test method parseBlockCss return array two items', async function () {
+    
+        let dataSiteRequestService = new DataSiteRequestService(); 
+        let scrapService = new ScrapService();
+        let body = await dataSiteRequestService.getResponseBodySite("https://www.w3.org/");
+
+        let links = await scrapService.parseBlockCss(body);
+        expect(links).to.not.null
+        expect(links[0].includes("css")).to.true;
     })
 })
