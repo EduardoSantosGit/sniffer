5a9dbfa65596b8bc45fee6d4c577ade76012004d
eduardosantosgit <eduardosantos058@gmail.com>
2018-09-25 20:47:42 -0300

    add test getasync notfound

--- test/infrastructure/client/HttpRequestClientTest.spec.js
+++ test/infrastructure/client/HttpRequestClientTest.spec.js
@@ -13,4 +13,14 @@ describe('HttpRequestClient client return httpresponse ok', function () {
         expect(result.statusCode).to.equal(200);
     })
 
+    it('test method getAsync returns response not found', async function () {
+    
+        let client = new HttpRequestClient();
+        let result = await client.getAsync("https://www.microsoft.com/pt-br/pt-ie/?rtc=1");
+
+        expect(result.headers).to.not.null;
+        expect(result.content).to.not.null;
+        expect(result.statusCode).to.equal(404);
+    })
+
 });
