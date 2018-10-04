fac744c1632a0d00bfde9945247077f7ddc61e2b
eduardosantosgit <eduardosantos058@gmail.com>
2018-10-04 15:00:00 +0100

    create first test method get ecommerce product

--- test/infrastructure/services/EcommerceService.spec.js
+++ test/infrastructure/services/EcommerceService.spec.js
@@ -1,8 +1,16 @@
+import EcommerceService from '../../../src/infrastructure/services/EcommerceService'
 import { expect } from 'chai'
 
 describe('Ecommerce Service', function () {
 
     it('test method getInfoEcommerce return html ecommerce', async function () {
+
+        let service = new EcommerceService();
+        let result = await service.getInfoEcommerce("submarino", "relogio");
         
+        expect(result).to.not.null
+        expect(result.status).to.equal("OK")
+        expect(result.value).to.not.null
+
     })
 })
