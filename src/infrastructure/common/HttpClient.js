c8f3c4df566c8e06305c29e16b90803a4c15ade1
Eduardo Santos <eduardosantos@MacBook-Pro-de-Mac.local>
2019-12-29 20:33:36 -0300

    alter method and names or comments not using codes

---
+++ src/infrastructure/common/HttpClient.js
@@ -0,0 +1,15 @@
+import request from 'request'
+
+export default class HttpClient {
+    
+    async sendAsync(req) {
+        return new Promise((resolve, reject) => {
+            request(req, (error, response, body) => {
+                if (error)
+                    reject(error)
+
+                resolve(response);
+            });
+        });
+    }
+}
--- src/infrastructure/services/ScrapService.js
+++ src/infrastructure/services/ScrapService.js
@@ -2,29 +2,42 @@ import Scrap from '../common/ScrapParser'
 
 export default class ScrapService {
 
-    constructor(){
+    constructor() {
         this.scrap = new Scrap()
     }
 
-    async parseBlockCss(text){
-        
+    async parseBlockCss(text) {
+
         let head = this.scrap.scrapBlockPage(text, "<head>", "</head>");
-        let tableLink = head.split("<link rel"); 
-        
+        let tableLink = head.split("<link rel");
+        console.log(tableLink.length);
         let ret = [];
-        tableLink.map(x => {
-            if(this.isCss(x))              
-                ret.push(this.formatLink(this.scrap.scrapBlockPage(x, "href=", "\">")))
-        });
+        
+        for (let index = 0; index < 1; index++) {
+            const element = tableLink[index];
+            console.log(element)
+            if (this.isCss(element)) {
+                //console.log("test " + JSON.stringify(this.formatLink(this.scrap.scrapBlockPage(element, "href=", "\">"))));
+                ret.push(this.formatLink(this.scrap.scrapBlockPage(element, "href=", "\">")))
+            }
+        }
+
+        // tableLink.map(x => {
+        //     if (this.isCss(x)) {
+        //         console.log(this.formatLink(this.scrap.scrapBlockPage(x, "href=", "\">")));
+        //         //ret.push(this.formatLink(this.scrap.scrapBlockPage(x, "href=", "\">")))
+        //     }
+        // });
+
         return ret
     }
 
-    isCss(link){
+    isCss(link) {
         return (link.includes(".css") == true) ? true : false;
     }
 
-    formatLink(link){
-        return link.replace(/\\/g,"").replace("\"","");
+    formatLink(link) {
+        return link.replace(/\\/g, "").replace("\"", "");
     }
 
 
